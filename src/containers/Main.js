dc69fa173591a5c66e449746505abca36338a751
seoyunho <yunho101775@gmail.com>
2017-12-18 00:54:47 +0900

    [CREATE] main containers

---
+++ src/containers/Main.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import HeaderNav from '../components/HeaderNav';
+import Footer from '../components/Footer';
+
+class Main extends Component{
+    render(){
+        return (
+            <div>
+                <HeaderNav/>
+                    <section>
+
+                    </section>
+                <Footer/>
+            </div> 
+        )
+    }
+}
+
+export default Booklist;
