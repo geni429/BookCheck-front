d1c6f1309ea7436a67549ced81887fdd2a2408dc
seoyunho <yunho101775@gmail.com>
2017-12-25 21:34:56 +0900

    [ADD] add search logic

--- src/components/SearchBook.js
+++ src/components/SearchBook.js
@@ -4,18 +4,34 @@ import '../css/SearchBook.css';
 class SearchBook extends Component{
   constructor(props){
     super(props);
+
+    this.state={
+      keyword:''
+    }
+
+    this.search=this.search.bind(this);
+    this.setKeyword=this.setKeyword.bind(this);
   }
   render(){
     return (
       <div id="search-wrapper">
         <div className="search-box" id="search-box">
-          <input className="search-box-text" id="search-box-input" type="text" placeholder="도서명 검색"/>
-          <input className="search-box-text" id="search-box-btn" type="submit" value="검색하기" onClick={this.search()}/>
+          <input className="search-box-text" id="search-box-input" type="text" placeholder="도서명 검색" onChange={this.setKeyword}/>
+          <input className="search-box-text" id="search-box-btn" type="submit" value="검색하기" onClick={this.search}/>
         </div>
       </div>
     )
   }
   search(){
+    console.log(this.state.keyword);
+  }
+
+  setKeyword(event){
+    let {value}=event.target;
+    this.setState({
+      ...this.state,
+      keyword:value
+    });
   }
 }
 
