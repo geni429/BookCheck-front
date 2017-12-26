d2b48b0d1295bbb4304ff0d385d1b15d55e2be47
geni429 <geni429@gmail.com>
2017-12-26 19:51:36 +0900

    [Modify]Delete JWT Token

--- src/containers/BookList.js
+++ src/containers/BookList.js
@@ -16,16 +16,12 @@ class BookList extends Component {
   componentDidMount() {
     let libraryID = 1234567890;
     let JWTtoken = 'JWT eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZGVudGl0eSI6Im1za2FuZzExNiIsIm5iZiI6MTUxNDI3Njc3NSwiaWF0IjoxNTE0Mjc2Nzc1LCJqdGkiOiJiZmI1YmYxYy04MDg4LTQ0NTItYTRiMy0xNzZjZDY4YzQ5NGQiLCJ0eXBlIjoiYWNjZXNzIiwiZXhwIjoxNTE0NTM1OTc1LCJmcmVzaCI6ZmFsc2V9.oAToTZ9NSJ6CazrWmk6KIhHQdFnahm09-KXBXOYzLDQ'
-    let url = '/book?library_id=' + libraryID + '?keyword=' + this.props.params.search;
+    let url = '/book?library_id=' + libraryID + '&keyword=' + this.props.params.search;
 
     axios({
       method: 'GET',
       url: url,
-      headers: {
-        'Authorization': JWTtoken
-      }
     }).then(res => {
-      console.log(res);
       this.setState({
         bookList: res.data
       });
